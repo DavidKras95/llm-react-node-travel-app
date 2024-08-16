b9665f0846b38c0cb0127016982503cf98e52c1e
david_kr <david_kr@optimove.com>
2024-08-16 15:05:46 +0300

    setup basic server api endpoint

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api", (req,res) =>{
+    res.json({test: ["test1", "test2", "test3"]});
+});
+
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+})
+
